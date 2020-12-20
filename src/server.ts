a2baee2ff66626b94c758a8761340bab17554d01
Klaudiusz Dembler <dev@kdembler.com>
2020-12-20 18:57:53 +0100

    switch to event-sourcing based data model

--- src/server.ts
+++ src/server.ts
@@ -5,14 +5,27 @@ import { connect } from 'mongoose'
 import { buildSchema } from 'type-graphql'
 import { VideoViewsInfosResolver } from './resolvers/videoViewsInfos'
 import config from './config'
+import { videoAggregate } from './aggregate'
 
 const main = async () => {
+  await getMongooseConnection()
+  await rebuildAggregates()
+
   const schema = await buildSchema({
     resolvers: [VideoViewsInfosResolver],
     emitSchemaFile: 'schema.graphql',
-    validate: true,
+    validate: false,
   })
 
+  const server = new ApolloServer({ schema })
+  const app = Express()
+  server.applyMiddleware({ app })
+  app.listen({ port: config.port }, () =>
+    console.log(`🚀 Server listening at ==> http://localhost:${config.port}${server.graphqlPath}`)
+  )
+}
+
+const getMongooseConnection = async () => {
   process.stdout.write(`Connecting to MongoDB at "${config.mongoDBUri}"...`)
   try {
     const mongoose = await connect(config.mongoDBUri, {
@@ -28,13 +41,19 @@ const main = async () => {
     return
   }
   process.stdout.write(' Done.\n')
+}
 
-  const server = new ApolloServer({ schema })
-  const app = Express()
-  server.applyMiddleware({ app })
-  app.listen({ port: config.port }, () =>
-    console.log(`🚀 Server listening at ==> http://localhost:${config.port}${server.graphqlPath}`)
-  )
+const rebuildAggregates = async () => {
+  process.stdout.write('Rebuilding video events aggregate...')
+  try {
+    await videoAggregate.rebuild()
+  } catch (error) {
+    process.stdout.write(' Failed!\n')
+    console.error(error)
+    process.exit()
+    return
+  }
+  process.stdout.write(' Done.\n')
 }
 
 main()
