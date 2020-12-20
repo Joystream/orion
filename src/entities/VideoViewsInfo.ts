e7830afba1aa2fa01d6073e42032318e712a668f
Klaudiusz Dembler <dev@kdembler.com>
2020-12-20 18:57:53 +0100

    switch to event-sourcing based data model

--- src/entities/VideoViewsInfo.ts
+++ src/entities/VideoViewsInfo.ts
@@ -1,15 +1,10 @@
 import { Field, ID, Int, ObjectType } from 'type-graphql'
-import { getModelForClass, prop } from '@typegoose/typegoose'
 
 @ObjectType()
 export class VideoViewsInfo {
   @Field(() => ID, { name: 'id' })
-  @prop({ required: true, index: true })
-  videoID: string
+  videoId: string
 
   @Field(() => Int)
-  @prop({ required: true, default: 0 })
   views: number
 }
-
-export const VideoViewsInfoModel = getModelForClass(VideoViewsInfo)
