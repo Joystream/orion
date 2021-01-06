e0c50245c056c236c0f2036fcabe709ce274efef
Klaudiusz Dembler <dev@kdembler.com>
2021-01-06 17:07:23 +0100

    add integration tests (#11)
    * refactor orion to be testable
    
    * add tests
    
    * add tests workflow on PR

--- src/aggregates/shared.ts
+++ src/aggregates/shared.ts
@@ -1,6 +1,5 @@
 import { GenericEvent } from '../models/shared'
 
-export abstract class GenericAggregate<EventType = GenericEvent> {
-  public abstract rebuild(): Promise<void>
-  public abstract applyEvent(event: EventType): void
+export interface GenericAggregate<EventType = GenericEvent> {
+  applyEvent: (event: EventType) => void
 }
