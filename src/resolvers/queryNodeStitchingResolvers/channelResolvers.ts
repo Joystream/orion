28e379b47a775265fbc651d55afd7c2476b29c75
drillprop <drylbartosz@gmail.com>
2021-12-07 14:28:07 +0100

    remove channels and channelsConnectionResolver, add promisingNewChannels and discoverNewChannels query

--- src/resolvers/queryNodeStitchingResolvers/channelResolvers.ts
+++ src/resolvers/queryNodeStitchingResolvers/channelResolvers.ts
@@ -1,52 +1,13 @@
 import type { IResolvers } from '@graphql-tools/utils'
 import { GraphQLSchema } from 'graphql'
 import { mapPeriods } from '../../helpers'
-import { Channel, ChannelEdge } from '../../types'
+import { ChannelEdge } from '../../types'
 import { getFollowsInfo, limitFollows } from '../followsInfo'
 import { getChannelViewsInfo, limitViews } from '../viewsInfo'
 import { createResolverWithTransforms, getSortedEntitiesBasedOnOrion } from './helpers'
 
 export const channelResolvers = (queryNodeSchema: GraphQLSchema): IResolvers => ({
   Query: {
-    channels: async (parent, args, context, info) => {
-      const chanelsResolver = createResolverWithTransforms(queryNodeSchema, 'channels')
-      const filterOrderByArgs = {
-        ...args,
-        orderBy: args.orderBy?.filter(
-          (orderBy: string) => orderBy !== 'followsNumber_ASC' && orderBy !== 'followsNumber_DESC'
-        ),
-      }
-      const channels = await chanelsResolver(parent, filterOrderByArgs, context, info)
-
-      const isSortedByFollows = args.orderBy?.find(
-        (orderByArg: string) => orderByArg === 'followsNumber_ASC' || orderByArg === 'followsNumber_DESC'
-      )
-
-      if (isSortedByFollows) {
-        const channelWithFollows = channels.map((channel: Channel) => ({
-          ...channel,
-          follows: getFollowsInfo(channel.id, context)?.follows || null,
-        }))
-        return [...channelWithFollows].sort((a, b) => {
-          return isSortedByFollows === 'followsNumber_DESC' ? b.follows - a.follows : a.follows - b.follows
-        })
-      }
-
-      const isSortedByViews = args.orderBy?.find(
-        (orderByArg: string) => orderByArg === 'viewsNumber_ASC' || orderByArg === 'viewsNumber_DESC'
-      )
-
-      if (isSortedByViews) {
-        const channelWithViews = channels.map((channel: Channel) => ({
-          ...channel,
-          views: getChannelViewsInfo(channel.id, context)?.views || null,
-        }))
-        return [...channelWithViews].sort((a, b) => {
-          return isSortedByViews === 'viewsNumber_DESC' ? b.views - a.views : a.views - b.views
-        })
-      }
-      return channels
-    },
     mostFollowedChannels: async (parent, args, context, info) => {
       context.followsAggregate.filterEventsByPeriod(args.timePeriodDays)
       const mostFollowedChannelIds = limitFollows(
@@ -79,62 +40,59 @@ export const channelResolvers = (queryNodeSchema: GraphQLSchema): IResolvers => 
 
       return getSortedEntitiesBasedOnOrion(parent, mostViewedChannelIds, context, info, queryNodeSchema, 'channels')
     },
-    channelsConnection: async (parent, args, context, info) => {
+    promisingNewChannels: async (parent, args, context, info) => {
       const channelsConnectionResolver = createResolverWithTransforms(queryNodeSchema, 'channelsConnection')
-      const filterOrderByArgs = {
-        ...args,
-        orderBy: args.orderBy?.filter(
-          (orderBy: string) =>
-            orderBy !== 'followsNumber_ASC' &&
-            orderBy !== 'followsNumber_DESC' &&
-            orderBy !== 'viewsNumber_ASC' &&
-            orderBy !== 'viewsNumber_DESC'
-        ),
-      }
-      const channelsConnection = await channelsConnectionResolver(parent, filterOrderByArgs, context, info)
-
-      const isSortedByFollows = args.orderBy?.find(
-        (orderByArg: string) => orderByArg === 'followsNumber_ASC' || orderByArg === 'followsNumber_DESC'
+      const channelsConnection = await channelsConnectionResolver(
+        parent,
+        {
+          orderBy: ['createdAt_DESC'],
+          first: 100,
+        },
+        context,
+        info
       )
 
-      if (isSortedByFollows) {
-        const nodesWithFollows = channelsConnection.edges.map((edge: ChannelEdge) => ({
-          ...edge,
-          node: {
-            ...edge.node,
-            follows: getFollowsInfo(edge.node.id, context)?.follows || null,
-          },
-        }))
-        return {
-          ...channelsConnection,
-          edges: [...nodesWithFollows].sort((a, b) => {
-            return isSortedByFollows === 'followsNumber_DESC'
-              ? b.node.follows - a.node.follows
-              : a.node.follows - b.node.follows
-          }),
-        }
-      }
+      const nodesWithViews = channelsConnection.edges.map((edge: ChannelEdge) => ({
+        ...edge,
+        node: {
+          ...edge.node,
+          views: getChannelViewsInfo(edge.node.id, context)?.views || null,
+        },
+      }))
 
-      const isSortedByViews = args.orderBy?.find(
-        (orderByArg: string) => orderByArg === 'viewsNumber_ASC' || orderByArg === 'viewsNumber_DESC'
+      return {
+        ...channelsConnection,
+        edges: [...nodesWithViews].sort((a, b) => {
+          return b.node.views - a.node.views
+        }),
+      }
+    },
+    discoverNewChannels: async (parent, args, context, info) => {
+      const channelsConnectionResolver = createResolverWithTransforms(queryNodeSchema, 'channelsConnection')
+      const channelsConnection = await channelsConnectionResolver(
+        parent,
+        {
+          orderBy: ['createdAt_DESC'],
+          first: 100,
+        },
+        context,
+        info
       )
-      if (isSortedByViews) {
-        const nodesWithViews = channelsConnection.edges.map((edge: ChannelEdge) => ({
-          ...edge,
-          node: {
-            ...edge.node,
-            views: getChannelViewsInfo(edge.node.id, context)?.views || null,
-          },
-        }))
-        return {
-          ...channelsConnection,
-          edges: [...nodesWithViews].sort((a, b) => {
-            return isSortedByViews === 'viewsNumber_DESC' ? b.node.views - a.node.views : a.node.views - b.node.views
-          }),
-        }
-      }
 
-      return channelsConnection
+      const nodesWithFollows = channelsConnection.edges.map((edge: ChannelEdge) => ({
+        ...edge,
+        node: {
+          ...edge.node,
+          follows: getFollowsInfo(edge.node.id, context)?.follows || null,
+        },
+      }))
+
+      return {
+        ...channelsConnection,
+        edges: [...nodesWithFollows].sort((a, b) => {
+          return b.node.follows - a.node.follows
+        }),
+      }
     },
   },
   Channel: {
