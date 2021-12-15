79aa55c1db07e6801234d2626151dc4423776c60
drillprop <drylbartosz@gmail.com>
2021-12-15 13:14:44 +0100

    bring back executor with logger

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,6 @@
 import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader'
 import { loadSchema } from '@graphql-tools/load'
 import { stitchSchemas } from '@graphql-tools/stitch'
-import { UrlLoader } from '@graphql-tools/url-loader'
 import { ApolloServerPluginLandingPageGraphQLPlayground, ContextFunction, PluginDefinition } from 'apollo-server-core'
 import { ApolloServer, ExpressContext } from 'apollo-server-express'
 import type { GraphQLRequestContext } from 'apollo-server-types'
@@ -17,19 +16,39 @@ import { FeaturedContentResolver } from './resolvers/featuredContent'
 import { queryNodeStitchingResolvers } from './resolvers/queryNodeStitchingResolvers'
 import { Aggregates, OrionContext } from './types'
 import config from './config'
+import type { ExecutionRequest } from '@graphql-tools/utils'
+import { print } from 'graphql'
+import { introspectSchema, wrapSchema } from '@graphql-tools/wrap'
+import fetch from 'cross-undici-fetch'
 
 export const createServer = async (mongoose: Mongoose, aggregates: Aggregates, queryNodeUrl: string) => {
   await mongoose.connection
+
+  const executor = async ({ document, variables, info }: ExecutionRequest) => {
+    const query = print(document)
+    const timeBeforeFetch = Date.now()
+    const fetchResult = await fetch(queryNodeUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ query, variables }),
+    })
+    const timeAfterFetch = Date.now()
+    console.log({ queryName: info?.fieldName, responseTime: timeAfterFetch - timeBeforeFetch })
+    return fetchResult.json()
+  }
+
+  const remoteQueryNodeSchema = wrapSchema({
+    schema: await introspectSchema(executor),
+    executor,
+  })
 
   const orionSchema = await buildSchema({
     resolvers: [VideoViewsInfosResolver, ChannelFollowsInfosResolver, FeaturedContentResolver],
     authChecker: customAuthChecker,
     emitSchemaFile: 'schema.graphql',
     validate: true,
-  })
-
-  const remoteQueryNodeSchema = await loadSchema(queryNodeUrl, {
-    loaders: [new UrlLoader()],
   })
 
   const queryNodeSchemaExtension = await loadSchema('./queryNodeSchemaExtension.graphql', {
