2b1e7e7c802d9a384808bafd257e25a9ffe843d9
drillprop <drylbartosz@gmail.com>
2021-12-17 15:54:09 +0100

    fix issue with not getting views or follows

--- src/server.ts
+++ src/server.ts
@@ -35,6 +35,7 @@ export const createServer = async (mongoose: Mongoose, aggregates: Aggregates, q
   const queryNodeSchemaExtension = await loadSchema('./queryNodeSchemaExtension.graphql', {
     loaders: [new GraphQLFileLoader()],
     schemas: [remoteQueryNodeSchema, orionSchema],
+    resolvers: queryNodeStitchingResolvers(remoteQueryNodeSchema),
   })
 
   const schema = stitchSchemas({
