568cc43e8699f848ee48825e8e9d136df1c5dd0d
Rafal Pawlow <pawlow.rafal@gmail.com>
2022-04-11 18:25:49 +0200

    PR FIX

--- src/resolvers/queryNodeStitchingResolvers/videoResolvers.ts
+++ src/resolvers/queryNodeStitchingResolvers/videoResolvers.ts
@@ -11,27 +11,13 @@ export const videoResolvers = (queryNodeSchema: GraphQLSchema): IResolvers<any, 
     top10VideosThisWeek: async (parent, args, context, info) => {
       const mostViewedVideosIds = getMostViewedVideosIds(context, { limit: 30, period: 7 })
       const resolver = createResolver(queryNodeSchema, 'videos')
-      const videos = await getDataWithIds(
-        resolver,
-        mostViewedVideosIds,
-        parent,
-        { ...args, isCensored_eq: false, isPublic_eq: true },
-        context,
-        info
-      )
+      const videos = await getDataWithIds(resolver, mostViewedVideosIds, parent, args, context, info)
       return sortEntities(videos, mostViewedVideosIds).slice(0, 10)
     },
     top10VideosThisMonth: async (parent, args, context, info) => {
       const mostViewedVideosIds = getMostViewedVideosIds(context, { limit: 30, period: 30 })
       const resolver = createResolver(queryNodeSchema, 'videos')
-      const videos = await getDataWithIds(
-        resolver,
-        mostViewedVideosIds,
-        parent,
-        { ...args, isCensored_eq: false, isPublic_eq: true },
-        context,
-        info
-      )
+      const videos = await getDataWithIds(resolver, mostViewedVideosIds, parent, args, context, info)
       return sortEntities(videos, mostViewedVideosIds).slice(0, 10)
     },
     mostViewedVideosConnection: async (parent, args, context, info) => {
