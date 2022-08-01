54c57a4b892cdfd7a74ddd754be36dd224f59761
drillprop <drylbartosz@gmail.com>
2022-08-01 13:10:06 +0200

    introduce basic filtering

--- src/entities/ReportVideoInfo.ts
+++ src/entities/ReportVideoInfo.ts
@@ -3,8 +3,17 @@ import { Field, ID, ObjectType } from 'type-graphql'
 @ObjectType()
 export class ReportVideoInfo {
   @Field(() => ID)
+  id: string
+
+  @Field(() => ID)
   videoId: string
 
   @Field()
   rationale: string
+
+  @Field()
+  createdAt: Date
+
+  @Field()
+  reporterIp: string
 }
--- src/resolvers/reportsInfo.ts
+++ src/resolvers/reportsInfo.ts
@@ -1,4 +1,17 @@
-import { Args, ArgsType, Ctx, Field, ID, Mutation, Query, Resolver } from 'type-graphql'
+import {
+  Args,
+  ArgsType,
+  Authorized,
+  Ctx,
+  Field,
+  ID,
+  InputType,
+  Int,
+  Mutation,
+  Query,
+  registerEnumType,
+  Resolver,
+} from 'type-graphql'
 import { ReportVideoInfo } from '../entities/ReportVideoInfo'
 import { ReportedVideoModel, saveReportedVideo } from '../models/ReportedContent'
 import { OrionContext } from '../types'
@@ -12,6 +25,43 @@ class ReportVideoArgs {
   rationale: string
 }
 
+enum Order {
+  ASC = 'asc',
+  DESC = 'desc',
+}
+
+registerEnumType(Order, { name: 'Order' })
+
+@InputType()
+class VideoReportWhereInput {
+  @Field(() => ID, { nullable: true })
+  videoId?: string
+
+  @Field(() => Date, { nullable: true })
+  createdAt_lt?: Date
+
+  @Field(() => Date, { nullable: true })
+  createdAt_gt?: Date
+
+  @Field({ nullable: true })
+  reporterIp: string
+}
+
+@ArgsType()
+class VideoReportsArgs {
+  @Field(() => VideoReportWhereInput, { nullable: true })
+  where: VideoReportWhereInput
+
+  @Field(() => Order, { nullable: true, defaultValue: Order.DESC })
+  order: Order
+
+  @Field(() => Int, { nullable: true, defaultValue: 30 })
+  limit: number
+
+  @Field(() => Int, { nullable: true })
+  skip: number
+}
+
 @Resolver()
 export class ReportsInfosResolver {
   @Mutation(() => ReportVideoInfo, { description: 'Report a video' })
@@ -19,26 +69,46 @@ export class ReportsInfosResolver {
     @Args() { videoId, rationale }: ReportVideoArgs,
     @Ctx() ctx: OrionContext
   ): Promise<ReportVideoInfo> {
-    saveReportedVideo({
+    const createdAt = new Date()
+    const reportedVideo = await saveReportedVideo({
       rationale,
       videoId,
-      reporterIp: ctx.remoteHost || ' ',
-      timestamp: new Date(),
+      reporterIp: ctx.remoteHost || '',
+      timestamp: createdAt,
     })
 
     return {
       rationale,
       videoId,
+      createdAt,
+      id: reportedVideo.id,
+      reporterIp: ctx.remoteHost || '',
     }
   }
-  @Query(() => [ReportVideoInfo], { description: 'Report a video' })
-  async reportedVideos(): Promise<ReportVideoInfo[]> {
-    const reportedVideosDocument = await ReportedVideoModel.find()
+
+  @Query(() => [ReportVideoInfo])
+  @Authorized()
+  async reportedVideos(
+    @Args() { order, where, limit, skip }: VideoReportsArgs,
+    @Ctx() ctx: OrionContext
+  ): Promise<ReportVideoInfo[]> {
+    const reportedVideosDocument = await ReportedVideoModel.find({
+      ...(where?.videoId ? { videoId: where?.videoId } : {}),
+      ...(where?.createdAt_gt ? { timestamp: { $gte: where.createdAt_gt } } : {}),
+      ...(where?.createdAt_lt ? { timestamp: { $lte: where.createdAt_lt } } : {}),
+      ...(where?.reporterIp ? { reporterIp: where?.reporterIp } : {}),
+    })
+      .skip(skip)
+      .limit(limit)
+      .sort({ timestamp: order })
 
     return (
       reportedVideosDocument.map((reportedVideo) => ({
         rationale: reportedVideo.rationale,
         videoId: reportedVideo.videoId,
+        createdAt: reportedVideo.timestamp,
+        id: reportedVideo.id,
+        reporterIp: ctx.remoteHost || '',
       })) || []
     )
   }
