83c697249dd75babd0719643bdabc678db057ba4
Lezek123 <leszek@jsgenesis.com>
2023-01-11 12:18:06 +0100

    channelNftCollectors query

--- src/server-extension/resolvers/ChannelsResolver/index.ts
+++ src/server-extension/resolvers/ChannelsResolver/index.ts
@@ -15,6 +15,7 @@ import {
   ChannelsSearchResult,
   ChannelFollowResult,
   ChannelUnfollowResult,
+  ChannelNftCollectorsOrderByInput,
 } from './types'
 import { GraphQLResolveInfo } from 'graphql'
 import { Context } from '@subsquid/openreader/lib/context'
@@ -23,6 +24,10 @@ import { randomAsHex } from '@polkadot/util-crypto'
 import { Report } from '../../../model/Report'
 import { extendClause } from '../../../utils/sql'
 import { buildExtendedChannelsQuery } from './utils'
+import { parseAnyTree } from '@subsquid/openreader/lib/opencrud/tree'
+import { getResolveTree } from '@subsquid/openreader/lib/util/resolve-tree'
+import { ListQuery } from '@subsquid/openreader/lib/sql/query'
+import { model } from '../model'
 
 @Resolver()
 export class ChannelsResolver {
@@ -75,10 +80,70 @@ export class ChannelsResolver {
 
   @Query(() => [ChannelNftCollector])
   async channelNftCollectors(
-    @Args() args: ChannelNftCollectorsArgs
+    @Args() args: ChannelNftCollectorsArgs,
+    @Info() info: GraphQLResolveInfo,
+    @Ctx() ctx: Context
   ): Promise<ChannelNftCollector[]> {
-    // TODO: Implement
-    return []
+    const tree = getResolveTree(info)
+
+    // Extract subsquid-supported Membership fields
+    const membershipSubTree = tree.fieldsByTypeName.ChannelNftCollector.member
+    const membershipFields = parseAnyTree(model, 'Membership', info.schema, membershipSubTree)
+
+    // Generate query using subsquid's ListQuery
+    const listQuery = new ListQuery(model, ctx.openreader.dialect, 'Membership', membershipFields, {
+      limit: args.limit,
+    })
+    let listQuerySql = listQuery.sql
+
+    listQuerySql = extendClause(listQuerySql, 'SELECT', '"collectors"."nft_count"')
+    listQuerySql = extendClause(
+      listQuerySql,
+      'FROM',
+      `
+        INNER JOIN (
+          SELECT
+              owned_nft.owner->>'member' AS member_id,
+              COUNT(owned_nft.id) AS nft_count
+          FROM
+              channel
+              INNER JOIN video ON video.channel_id = channel.id
+              INNER JOIN owned_nft ON owned_nft.video_id = video.id
+          WHERE channel.id = $${listQuery.params.length + 1}
+          GROUP BY owned_nft.owner->>'member'
+          HAVING COUNT(owned_nft.id) > 0
+        ) AS collectors ON collectors.member_id = membership.id
+      `,
+      ''
+    )
+    listQuery.params.push(args.channelId)
+
+    if (args.orderBy !== undefined) {
+      listQuerySql = extendClause(
+        listQuerySql,
+        'ORDER BY',
+        args.orderBy === ChannelNftCollectorsOrderByInput.amount_ASC
+          ? '"nft_count" ASC'
+          : '"nft_count" DESC'
+      )
+    }
+
+    ;(listQuery as { sql: string }).sql = listQuerySql
+
+    const oldListQMap = listQuery.map.bind(listQuery)
+    listQuery.map = (rows: any[][]) => {
+      const nftCounts: string[] = []
+      for (const row of rows) {
+        nftCounts.push(row.pop())
+      }
+      const membersMapped = oldListQMap(rows)
+      return membersMapped.map((member, i) => ({ member, amount: nftCounts[i] }))
+    }
+
+    const result = await ctx.openreader.executeQuery(listQuery)
+    console.log('Result', result)
+
+    return result
   }
 
   @Query(() => [ChannelsSearchResult!])
