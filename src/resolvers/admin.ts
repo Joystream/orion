a79cd449f2c2082a351e94a6117a0e55621a61e2
attemka <attemka@gmail.com>
2023-02-24 12:22:35 +0300

    App-actions functionality (#75)
    * add support for AppAction signatures (#51)
    
    fixes https://github.com/Joystream/orion/issues/50
    
    * New way to generate commitment and algorithm switch (#66)
    
    * New way to generate commitment and algorithm switch
    
    * Comment update
    
    * Update secret key comments
    
    Co-authored-by: l1.media <31551045+traumschule@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: l1.media <31551045+traumschule@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: WRadoslaw <92513933+WRadoslaw@users.noreply.github.com>
    Co-authored-by: l1.media <31551045+traumschule@users.noreply.github.com>

--- src/resolvers/admin.ts
+++ src/resolvers/admin.ts
@@ -1,11 +1,26 @@
-import { Authorized, Field, Mutation, Query, Resolver, Args, ArgsType } from 'type-graphql'
-import { getAdminDoc, Admin } from '../models/Admin'
-import { ADMIN_ROLE } from '../config'
+import { Args, ArgsType, Authorized, Field, Mutation, Query, Resolver } from 'type-graphql'
+import { Admin, GeneratedSignature, getAdminDoc } from '../models/Admin'
+import config, { ADMIN_ROLE } from '../config'
+import { ed25519Sign } from '@polkadot/util-crypto'
+import { u8aToHex, hexToU8a, isHex } from '@polkadot/util'
+import { generateAppActionCommitment } from './helpers'
 
 @ArgsType()
 class AdminInput implements Admin {
   @Field()
   isKilled: boolean
+}
+
+@ArgsType()
+class AppActionSignatureInput {
+  @Field()
+  creatorId: string
+  @Field({ description: 'Hex string from UInt8Array' })
+  assets: string
+  @Field({ description: 'Hex string from UInt8Array' })
+  rawAction: string
+  @Field({ description: 'Hex string from UInt8Array' })
+  rawAppActionMetadata: string
 }
 
 @Resolver()
@@ -22,5 +37,23 @@ export class AdminResolver {
     killSwitch.isKilled = isKilled
     await killSwitch.save()
     return { isKilled }
+  }
+
+  @Mutation(() => GeneratedSignature)
+  async signAppActionCommitment(
+    @Args() { rawAppActionMetadata, rawAction, assets, creatorId }: AppActionSignatureInput
+  ) {
+    if (!isHex(assets) || !isHex(rawAction) || !isHex(rawAppActionMetadata)) {
+      throw new Error('One of input is not hex: assets, rawAction, rawAppActionMetadata')
+    }
+
+    const message = generateAppActionCommitment(
+      creatorId,
+      hexToU8a(assets),
+      hexToU8a(rawAction),
+      hexToU8a(rawAppActionMetadata)
+    )
+    const signature = ed25519Sign(message, config.appKeypair)
+    return { signature: u8aToHex(signature) }
   }
 }
