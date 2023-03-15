330f1c69d472f8cd2160333431eb8f6c9602e628
Lezek123 <leszek@jsgenesis.com>
2023-03-15 10:55:08 +0100

    Excluding/restoring comments: Update: - parentComment.repliesCount, - parentComment.reactionsAndRepliesCount, - video.commentsCount

--- src/server-extension/resolvers/AdminResolver/utils.ts
+++ src/server-extension/resolvers/AdminResolver/utils.ts
@@ -1,17 +1,25 @@
-import { stringToHex, u8aToHex } from '@polkadot/util'
+import { EntityManager, In } from 'typeorm'
+import { Comment, Video } from '../../../model'
 
-// TODO: preferably this would be imported from @joystream/js -> https://github.com/Joystream/joystream/pull/4586
-export const generateAppActionCommitment = (
-  creatorId: string,
-  assets: Uint8Array,
-  rawAction: Uint8Array,
-  rawAppActionMetadata: Uint8Array
-): string => {
-  const rawCommitment = [
-    creatorId,
-    u8aToHex(assets),
-    u8aToHex(rawAction),
-    u8aToHex(rawAppActionMetadata),
-  ]
-  return stringToHex(JSON.stringify(rawCommitment))
+export async function processCommentsCensorshipStatusUpdate(
+  em: EntityManager,
+  ids: string[],
+  censored: boolean
+) {
+  const comments = await em.getRepository(Comment).find({ where: { id: In(ids) } })
+  const videoIdsAffected = comments.map((c) => c.videoId)
+  const parentCommentIdsAffected = comments.map((c) => c.parentCommentId)
+  const operation = censored ? '-' : '+'
+  // Reduce/increase videos' commentsCount
+  await em
+    .getRepository(Video)
+    .update({ id: In(videoIdsAffected) }, { commentsCount: () => `comments_count ${operation} 1` })
+  // Reduce/increase parent comments' replies count
+  await em.getRepository(Comment).update(
+    { id: In(parentCommentIdsAffected) },
+    {
+      repliesCount: () => `replies_count ${operation} 1`,
+      reactionsAndRepliesCount: () => `reactions_and_replies_count ${operation} 1`,
+    }
+  )
 }
