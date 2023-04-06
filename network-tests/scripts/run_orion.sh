40e3f294e987d693bfa6822f7b248c959a604c4e
Ignazio Bovo <ignazio@jsgenesis.com>
2023-04-06 09:54:28 +0200

    feat: add stop script for orion

--- network-tests/scripts/run_orion.sh
+++ network-tests/scripts/run_orion.sh
@@ -1,7 +1,10 @@
-
 #!/usr/bin/env bash
 set -e
 
 SCRIPT_PATH="$(dirname "${BASH_SOURCE[0]}")"
 cd $SCRIPT_PATH
+
+docker network create joystream_default || true
+docker-compose -f ../../docker-compose.yml up -d
+docker-compose -f ../../archive/docker-compose.yml up -d
 
