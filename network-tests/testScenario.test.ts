d4d7984dda3bb0fd22ee3de4ed452311d9ef148f
Ignazio Bovo <ignazio@jsgenesis.com>
2023-04-11 19:46:48 +0200

    feat: add apollo client

--- network-tests/testScenario.test.ts
+++ network-tests/testScenario.test.ts
@@ -2,27 +2,37 @@ import { ApiPromise, WsProvider } from '@polkadot/api'
 import { afterAll, beforeAll, describe, expect, it } from "@jest/globals"
 import { waitMilliSec } from "./testUtils"
 import { JsNodeApi } from './joystreamNodeApi'
+import {
+  ApolloClient,
+  InMemoryCache,
+  NormalizedCacheObject,
+} from '@apollo/client/core'
+
 import dotenv from 'dotenv';
 dotenv.config();
 
 const env = process.env
 let jsNode: JsNodeApi;
 let provider: WsProvider;
+let client: ApolloClient<NormalizedCacheObject>
 
 beforeAll(async () => {
   if (env.TREASURY_ACCOUNT_URI === undefined) {
     process.exit(-1)
   }
 
-  // try {
+  // connect Joystream Node
   provider = new WsProvider("ws://127.0.0.1:9944")
   const api = await ApiPromise.create({ provider })
   await api.isReadyOrError
   jsNode = new JsNodeApi(env.TREASURY_ACCOUNT_URI!, api)
-    // await waitMilliSec(5000) // in order for the node to start producing blocks
-  // } catch (error) {
-  //   console.error(error)
-  // }
+  await waitMilliSec(5000) // in order for the node to start producing blocks
+
+  // connect to Apollo Client
+  client = new ApolloClient({
+    cache: new InMemoryCache({ addTypename: false }),
+    defaultOptions: { query: { fetchPolicy: 'no-cache', errorPolicy: 'all' } },
+  })
 
 })
 
@@ -34,5 +44,6 @@ describe("testing jsApi", () => {
 })
 
 afterAll(async () => {
-  provider.disconnect().catch(() => {})
+  provider.disconnect().catch(() => { })
+  client.stop()
 })
