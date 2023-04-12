3bf8a55f03204c149b9bfef7657bb5211e0fe2ae
Ignazio Bovo <ignazio@jsgenesis.com>
2023-04-12 18:00:43 +0200

    feat: add test example for orion

--- network-tests/testScenario.test.ts
+++ network-tests/testScenario.test.ts
@@ -1,5 +1,6 @@
 import { afterAll, beforeAll, describe, expect, it } from '@jest/globals'
 import { TestContext } from './testUtils'
+import { gql } from "@apollo/client/core"
 
 import dotenv from 'dotenv'
 dotenv.config()
@@ -25,6 +26,20 @@ afterAll(async () => {
 describe('testing jsApi', () => {
   it('initial council is empty', async () => {
     const councilMembers = await ctx.jsNodeApi.query.council.councilMembers()
-    expect(councilMembers.toArray()).toEqual([])
+    expect(councilMembers.toArray()).toHaveLength(0)
+  })
+
+  it('get members is empty at js node start', async () => {
+    const GET_MEMBERS = gql`
+      query MyQuery($id_in: [String!] = ["1"], $handle_in: [String!] = "") {
+        memberships(where: {id_in: $id_in, OR: {handle_in: $handle_in}}) {
+            id
+            handle
+        }
+      }
+    `
+    const res = await ctx.orionClient.query({ query: GET_MEMBERS, variables: {id_in: ["1"], handle_in: ["test_handle1", "test_handle2"]} })
+    expect(res.data).toBeTruthy()
+    expect(res.data.memberships).toHaveLength(0)
   })
 })
