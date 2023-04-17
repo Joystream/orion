2773393a4a944ef62a8381fc562db4eea949858d
Ignazio Bovo <ignazio@jsgenesis.com>
2023-04-17 20:06:35 +0200

    fix: fix scenario selection

--- network-tests/src/scenarios/creatorToken.ts
+++ network-tests/src/scenarios/creatorToken.ts
@@ -1,9 +1,7 @@
 import electCouncil from '../flows/council/elect'
-import failToElectCouncil from '../flows/council/failToElect'
 import { scenario } from '../Scenario'
-import failToElectWithBlacklist from '../flows/council/electWithBlacklist'
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 scenario('Creator Token Test Suite', async ({ job }) => {
-  const councilJob = job('electing council', electCouncil)
+  job('electing council', electCouncil)
 })
