cb6d6cb37b9bc102a10b2ba1c412ce2c518a6f6b
Ignazio Bovo <ignazio@jsgenesis.com>
2023-04-18 08:50:50 +0200

    fix: added types debug

--- network-tests/src/scenarios/creatorToken.ts
+++ network-tests/src/scenarios/creatorToken.ts
@@ -1,7 +1,10 @@
+import leadOpening from '../flows/working-groups/leadOpening'
 import electCouncil from '../flows/council/elect'
 import { scenario } from '../Scenario'
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 scenario('Creator Token Test Suite', async ({ job }) => {
-  job('electing council', electCouncil)
+  job('hire leads', leadOpening(true, ['contentWorkingGroup'])).requires(
+    job('electing council', electCouncil)
+  )
 })
