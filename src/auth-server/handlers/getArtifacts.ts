d252937592e04ae96cbcab4f79bf979fab4a8e28
Lezek123 <leszek@jsgenesis.com>
2023-04-25 13:55:29 +0200

    Auth API: Encryption artifacts and related tests

--- src/auth-server/handlers/getArtifacts.ts
+++ src/auth-server/handlers/getArtifacts.ts
@@ -1,10 +1,31 @@
 import express from 'express'
-import { AuthApiError } from '../errors'
+import { NotFoundError } from '../errors'
+import { globalEm } from '../../utils/globalEm'
+import { EncryptionArtifacts } from '../../model'
+import { components, operations } from '../generated/api-types'
 
+type ReqParams = Record<string, unknown>
+type ReqBody = unknown
+type ReqQuery = operations['getArtifacts']['parameters']['query']
+type ResBody =
+  | components['schemas']['EncryptionArtifacts']
+  | components['schemas']['GenericErrorResponseData']
+
+// TODO: Rate limiting, also for other endpoints
 export const getArtifacts: (
-  req: express.Request,
-  res: express.Response,
+  req: express.Request<ReqParams, ResBody, ReqBody, ReqQuery>,
+  res: express.Response<ResBody>,
   next: express.NextFunction
 ) => Promise<void> = async (req, res, next) => {
-  return next(new AuthApiError('Unimplemented', 501))
+  try {
+    const em = await globalEm
+    const { id } = req.query
+    const artifacts = await em.getRepository(EncryptionArtifacts).findOneBy({ id })
+    if (!artifacts) {
+      throw new NotFoundError('Artifacts not found by provided id (lookupKey)')
+    }
+    res.status(200).json(artifacts)
+  } catch (e) {
+    next(e)
+  }
 }
--- src/auth-server/handlers/postArtifacts.ts
+++ src/auth-server/handlers/postArtifacts.ts
@@ -1,10 +1,31 @@
 import express from 'express'
-import { AuthApiError } from '../errors'
+import { globalEm } from '../../utils/globalEm'
+import { EncryptionArtifacts } from '../../model'
+import { components } from '../generated/api-types'
 
+type ReqParams = Record<string, string>
+type ResBody =
+  | components['schemas']['GenericOkResponseData']
+  | components['schemas']['GenericErrorResponseData']
+type ReqBody = components['schemas']['EncryptionArtifacts']
+
+// TODO: Rate limiting, also for other endpoints
 export const postArtifacts: (
-  req: express.Request,
-  res: express.Response,
+  req: express.Request<ReqParams, ResBody, ReqBody>,
+  res: express.Response<ResBody>,
   next: express.NextFunction
 ) => Promise<void> = async (req, res, next) => {
-  return next(new AuthApiError('Unimplemented', 501))
+  try {
+    const em = await globalEm
+    const { encryptedSeed, cipherIv, id } = req.body
+    const encryptionArtifacts = new EncryptionArtifacts({
+      id,
+      cipherIv,
+      encryptedSeed,
+    })
+    await em.save(encryptionArtifacts)
+    res.status(200).json({ success: true })
+  } catch (e) {
+    next(e)
+  }
 }
