079307365e2dcf8920ea63a8ec105c22be329d5c
Ignazio Bovo <ignazio@jsgenesis.com>
2023-05-08 15:57:56 +0200

    feat: add amm flow

---
+++ network-tests/src/flows/token/amm.ts
@@ -0,0 +1,38 @@
+import { FlowProps } from '../../Flow'
+import { extendDebug } from '../../Debugger'
+import { FixtureRunner } from '../../Fixture'
+import { expect } from 'chai'
+import { Resource } from '../../Resources'
+import { BN } from 'bn.js'
+import { ActivateAmmFixture } from '../../fixtures/token'
+
+export default async function amm({ api, query, lock }: FlowProps): Promise<void> {
+  const debug = extendDebug('flow:token-sale')
+  debug('Started')
+  api.enableDebugTxLogs()
+
+  const tokenId = (await api.query.projectToken.nextTokenId()).toNumber() - 1
+  const channelId = (await api.query.content.nextChannelId()).toNumber() - 1
+  expect(tokenId).gte(1)
+  expect(channelId).gte(1)
+
+  // retrieve owner info
+  const unlockCreatorAccess = await lock(Resource.Creator)
+  const [creatorAddress, creatorMemberId] = api.creator
+  unlockCreatorAccess()
+
+  const unlockFirstHolderAccess = await lock(Resource.FirstHolder)
+  const [firstHolderAddress, firstHolderId] = api.firstHolder
+  unlockFirstHolderAccess()
+
+  // sale params
+  debug('issue token sale')
+  const ammParams = api.createType('PalletProjectTokenAmmParams', {
+        slope: api.createType('Permill', new BN(10)),
+        intercept: api.createType('Permill', new BN(10000))
+  })
+  const activateAmmFixture = new ActivateAmmFixture(api, query, creatorAddress, creatorMemberId, channelId, ammParams)
+  await new FixtureRunner(activateAmmFixture).run()
+}
+
+
