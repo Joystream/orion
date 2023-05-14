62ef191e46260bf86d1c2e675cb46dc8417a432b
Ignazio Bovo <ignazio@jsgenesis.com>
2023-05-14 17:50:05 +0200

    feat: finish JoinWhitelist fixture

--- network-tests/src/fixtures/token/JoinWhitelistFixture.ts
+++ network-tests/src/fixtures/token/JoinWhitelistFixture.ts
@@ -5,14 +5,18 @@ import { SubmittableResult } from '@polkadot/api'
 import { OrionApi } from '../../OrionApi'
 import { Api } from '../../Api'
 import { PalletProjectTokenMerkleProof } from '@polkadot/types/lookup'
+import { assert } from 'chai'
+import BN from 'bn.js'
 
 type MemberJoinedWhitelistEventDetails = EventDetails<EventType<'projectToken', 'MemberJoinedWhitelist'>>
 
 export class JoinWhitelistFixture extends StandardizedFixture {
-  protected memberId: number 
+  protected memberId: number
   protected memberAddress: string
   protected tokenId: number
+  protected events: MemberJoinedWhitelistEventDetails[] = []
   protected proof: PalletProjectTokenMerkleProof
+  protected tokenAccountNumberPre: number | undefined
 
   public constructor(
     api: Api,
@@ -23,9 +27,9 @@ export class JoinWhitelistFixture extends StandardizedFixture {
     proof: PalletProjectTokenMerkleProof
   ) {
     super(api, query)
-    this.memberId = memberId 
-    this.memberAddress = memberAddress 
-    this.tokenId = tokenId 
+    this.memberId = memberId
+    this.memberAddress = memberAddress
+    this.tokenId = tokenId
     this.proof = proof
   }
 
@@ -41,12 +45,32 @@ export class JoinWhitelistFixture extends StandardizedFixture {
     return this.api.getEventDetails(result, 'projectToken', 'MemberJoinedWhitelist')
   }
 
-  public async tryQuery(): Promise<void> {
-    const token = await this.query.getTokenById(this.api.createType('u64', 0))
-    console.log(`Query result:\n ${token}`)
+  public async preExecHook(): Promise<void> {
+    const _tokenId = this.api.createType('u64', this.tokenId)
+    const qToken = await this.query.retryQuery(() => this.query.getTokenById(_tokenId))
+    assert.isNotNull(qToken)
+    this.tokenAccountNumberPre = qToken!.accountsNum
   }
 
   public assertQueryNodeEventIsValid(qEvent: AnyQueryNodeEvent, i: number): void {
+  }
+
+  public async runQueryNodeChecks(): Promise<void> {
+    const [tokenId, memberId] = this.events[0].event.data
+    const nodeAccount = await this.api.query.projectToken.accountInfoByTokenAndMember(tokenId, memberId)
+    const qToken = await this.query.retryQuery(() => this.query.getTokenById(tokenId))
+    const qAccount = await this.query.retryQuery(() => this.query.getTokenAccountById(tokenId.toString() + memberId.toString()))
+    const tokenAccountNumberPost = this.tokenAccountNumberPre! + 1
+
+    assert.isNotNull(qAccount)
+    assert.isNotNull(qToken)
+    assert.equal(qToken!.accountsNum, tokenAccountNumberPost)
+    assert.equal(qAccount!.totalAmount, nodeAccount.amount.toString())
+    assert.equal(qAccount!.member.id, memberId.toString())
+    assert.equal(qAccount!.stakedAmount, '0')
+    assert.equal(qAccount!.token.id, tokenId.toString())
+    assert.equal(qAccount!.whitelisted!, true)
+    assert.equal(qAccount!.deleted, false)
   }
 }
 
