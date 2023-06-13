8573e4402362b17d684c98cd5f97f0044dafa2b7
Ignazio Bovo <ignazio@jsgenesis.com>
2023-06-13 08:33:14 +0200

    fix: migration ok

--- src/utils/overlay.ts
+++ src/utils/overlay.ts
@@ -9,7 +9,6 @@ import _, { isObject } from 'lodash'
 import { NextEntityId } from '../model/NextEntityId'
 import { criticalError } from './misc'
 import { Logger } from '../logger'
-import { Token, TokenAccount } from '../model'
 
 // A stub which can represent any entity type
 export type AnyEntity = { id: string }
