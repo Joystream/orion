0433b2fc32fb810685265239b69c14cb82ef442c
Ignazio Bovo <ignazio@jsgenesis.com>
2023-06-26 12:53:15 +0200

    feat: sketch mutation for setting the prefeences

--- src/server-extension/resolvers/AccountResolver/index.ts
+++ src/server-extension/resolvers/AccountResolver/index.ts
@@ -1,18 +1,18 @@
 import 'reflect-metadata'
-import { Query, Resolver, UseMiddleware, Ctx, Info } from 'type-graphql'
+import { Query, Resolver, Mutation, UseMiddleware, Ctx, Info, Args } from 'type-graphql'
 import { EntityManager } from 'typeorm'
 import { AccountOnly } from '../middleware'
-import { AccountData, FollowedChannel } from './types'
+import { AccountAndNotificationPreferences, AccountData, FollowedChannel, SetNotificationPreferencesArgs } from './types'
 import { Context } from '../../check'
 import { GraphQLResolveInfo } from 'graphql'
 import assert from 'assert'
 import { withHiddenEntities } from '../../../utils/sql'
-import { ChannelFollow } from '../../../model'
+import { ChannelFollow, NotificationPreferences } from '../../../model'
 
 @Resolver()
 export class AccountResolver {
   // Set by depenency injection
-  constructor(private em: () => Promise<EntityManager>) {}
+  constructor(private em: () => Promise<EntityManager>) { }
 
   @UseMiddleware(AccountOnly)
   @Query(() => AccountData)
@@ -45,6 +45,57 @@ export class AccountResolver {
       membershipId,
       isEmailConfirmed,
       followedChannels,
+    }
+  }
+
+
+  @Mutation(() => AccountAndNotificationPreferences)
+  @UseMiddleware(AccountOnly)
+  async setAccountNotificatioPreferences(
+    @Args() notificationPreferences: SetNotificationPreferencesArgs,
+    @Ctx() ctx: Context
+  ): Promise<AccountAndNotificationPreferences> {
+    const account = ctx.account
+    assert(account, 'Unexpected context: account is not set')
+    const {
+      councilElectionUpdates,
+      updatesInProposalCreated,
+      discussionThreadPosted,
+      postDirectMention,
+      notificationWithRole,
+      notificationWithoutRole,
+      forumDirectMention,
+      forumReplyToPost,
+      councilElectionUpdatesMail,
+      updatesInProposalCreatedMail,
+      discussionThreadPostedMail,
+      postDirectMentionMail,
+      notificationWithRoleMail,
+      notificationWithoutRoleMail,
+      forumDirectMentionMail,
+      forumReplyToPostMail
+    } = notificationPreferences
+
+    account.notificationPreferences.councilElectionUpdates = councilElectionUpdates
+    account.notificationPreferences.updatesInProposalCreated = updatesInProposalCreated
+    account.notificationPreferences.discussionThreadPosted = discussionThreadPosted
+    account.notificationPreferences.postDirectMention = postDirectMention
+    account.notificationPreferences.notificationWithRole = notificationWithRole
+    account.notificationPreferences.notificationWithoutRole = notificationWithoutRole
+    account.notificationPreferences.forumDirectMention = forumDirectMention
+    account.notificationPreferences.forumReplyToPost = forumReplyToPost
+    account.notificationPreferences.councilElectionUpdatesMail = councilElectionUpdatesMail
+    account.notificationPreferences.updatesInProposalCreatedMail = updatesInProposalCreatedMail
+    account.notificationPreferences.discussionThreadPostedMail = discussionThreadPostedMail
+    account.notificationPreferences.postDirectMentionMail = postDirectMentionMail
+    account.notificationPreferences.notificationWithRoleMail = notificationWithRoleMail
+    account.notificationPreferences.notificationWithoutRoleMail = notificationWithoutRoleMail
+    account.notificationPreferences.forumDirectMentionMail = forumDirectMentionMail
+    account.notificationPreferences.forumReplyToPostMail = forumReplyToPostMail
+
+    return {
+      accountId: account.id,
+      preferences: account.notificationPreferences,
     }
   }
 }
--- src/server-extension/resolvers/AccountResolver/types.ts
+++ src/server-extension/resolvers/AccountResolver/types.ts
@@ -1,4 +1,5 @@
-import { Field, ObjectType } from 'type-graphql'
+import { Field, ObjectType, ArgsType } from 'type-graphql'
+import { NotificationPreferences } from '../../../model'
 
 @ObjectType()
 export class FollowedChannel {
@@ -28,4 +29,64 @@ export class AccountData {
 
   @Field(() => [FollowedChannel], { nullable: false })
   followedChannels: FollowedChannel[]
+}
+
+@ArgsType()
+export class SetNotificationPreferencesArgs {
+  @Field(() => Boolean, { nullable: false })
+  councilElectionUpdates: boolean
+
+  @Field(() => Boolean, { nullable: false })
+  updatesInProposalCreated: boolean
+
+  @Field(() => Boolean, { nullable: false })
+  discussionThreadPosted: boolean
+
+  @Field(() => Boolean, { nullable: false })
+  postDirectMention: boolean
+
+  @Field(() => Boolean, { nullable: false })
+  notificationWithRole: boolean
+
+  @Field(() => Boolean, { nullable: false })
+  notificationWithoutRole: boolean
+
+  @Field(() => Boolean, { nullable: false })
+  forumDirectMention: boolean
+
+  @Field(() => Boolean, { nullable: false })
+  forumReplyToPost: boolean
+
+  @Field(() => Boolean, { nullable: false })
+  councilElectionUpdatesMail: boolean
+
+  @Field(() => Boolean, { nullable: false })
+  updatesInProposalCreatedMail: boolean
+
+  @Field(() => Boolean, { nullable: false })
+  discussionThreadPostedMail: boolean
+
+  @Field(() => Boolean, { nullable: false })
+  postDirectMentionMail: boolean
+
+  @Field(() => Boolean, { nullable: false })
+  notificationWithRoleMail: boolean
+
+  @Field(() => Boolean, { nullable: false })
+  notificationWithoutRoleMail: boolean
+
+  @Field(() => Boolean, { nullable: false })
+  forumDirectMentionMail: boolean
+
+  @Field(() => Boolean, { nullable: false })
+  forumReplyToPostMail: boolean
+}
+
+@ObjectType()
+export class AccountAndNotificationPreferences {
+  @Field(() => String, { nullable: false })
+  accountId: string 
+
+  @Field(() => NotificationPreferences, { nullable: false })
+  preferences: NotificationPreferences
 }
