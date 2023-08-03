08ef1e8609f2cee790e15a281fecb8ed967561c1
Ignazio Bovo <ignazio@jsgenesis.com>
2023-08-03 18:05:52 +0200

    feat: :art: add notification links

---
+++ src/utils/notification/notificationLinks.ts
@@ -0,0 +1,85 @@
+import { ConfigVariable, config } from '../config'
+import { EntityManager } from 'typeorm'
+
+// expected like "gleev.xyz"
+const getRootDomain = async (em: EntityManager) => config.get(ConfigVariable.AppRootDomain, em)
+
+export const channelExcludedLink = async (em: EntityManager) =>
+  `https://${await getRootDomain(em)}/legal/tos`
+
+export const videoExcludedLink = async (em: EntityManager) =>
+  `https://${await getRootDomain(em)}/legal/tos`
+
+export const videoFeaturedOnCategoryPageLink = async (em: EntityManager, categoryId: string) =>
+  `https://${await getRootDomain(em)}/category/${categoryId}`
+
+export const videoFeaturedAsCategoryHeroLink = async (em: EntityManager, categoryId: string) =>
+  `https://${await getRootDomain(em)}/category/${categoryId}`
+
+export const newChannelFollowerLink = async (em: EntityManager, memberHandle: string) =>
+  `https://${await getRootDomain(em)}/${memberHandle}`
+
+// FIXME: edit link such that the page is focused to comment
+export const commentPostedToVideoLink = async (em: EntityManager, videoId: string) =>
+  `https://${await getRootDomain(em)}/video/${videoId}`
+
+export const videoLikedLink = async (em: EntityManager, videoId: string) =>
+  `https://${await getRootDomain(em)}/video/${videoId}`
+
+export const videoDislikedLink = async (em: EntityManager, videoId: string) =>
+  `https://${await getRootDomain(em)}/video/${videoId}`
+
+export const channelVerifiedLink = async (em: EntityManager) =>
+  `https://${await getRootDomain(em)}/ypp`
+
+export const yppSignupSuccessfulLink = async (em: EntityManager) =>
+  `https://${await getRootDomain(em)}/ypp`
+
+export const channelSuspendedLink = async (em: EntityManager) =>
+  `https://${await getRootDomain(em)}/ypp`
+
+export const nftSoldLink = async (em: EntityManager, videoId: string) =>
+  `https://${await getRootDomain(em)}/video/${videoId}`
+
+export const royaltiesReceivedLink = async (em: EntityManager, videoId: string) =>
+  `https://${await getRootDomain(em)}/video/${videoId}`
+
+export const directPaymentByMemberLink = async (em: EntityManager, memberHandle: string) =>
+  `https://${await getRootDomain(em)}/member/${memberHandle}`
+
+// design specifies no-redirect
+export const ChannelFundsWithdrawn = () => ''
+
+// FIXME: edit link such that the page is focused to comment
+export const commentReplyLink = async (em: EntityManager, videoId: string) =>
+  `https://${await getRootDomain(em)}/video/${videoId}`
+
+export const commentReactionLink = async (em: EntityManager, videoId: string) =>
+  `https://${await getRootDomain(em)}/video/${videoId}`
+
+export const newVideoPostedLink = async (em: EntityManager, videoId: string) =>
+  `https://${await getRootDomain(em)}/video/${videoId}`
+
+export const nftOnSaleLink = async (em: EntityManager, videoId: string) =>
+  `https://${await getRootDomain(em)}/video/${videoId}`
+
+export const nftOnAuction = async (em: EntityManager, videoId: string) =>
+  `https://${await getRootDomain(em)}/video/${videoId}`
+
+export const hiherBidPlacedLink = async (em: EntityManager, videoId: string) =>
+  `https://${await getRootDomain(em)}/video/${videoId}`
+
+export const timedAuctionWonLink = async (em: EntityManager, videoId: string) =>
+  `https://${await getRootDomain(em)}/video/${videoId}`
+
+export const openAuctionWonLink = async (em: EntityManager, videoId: string) =>
+  `https://${await getRootDomain(em)}/video/${videoId}`
+
+export const timedAuctionLostLink = async (em: EntityManager, videoId: string) =>
+  `https://${await getRootDomain(em)}/video/${videoId}`
+
+export const openAuctionLostLink = async (em: EntityManager, videoId: string) =>
+  `https://${await getRootDomain(em)}/video/${videoId}`
+
+export const channelCreatedLink = async (em: EntityManager, channelId: string) =>
+  `https://${await getRootDomain(em)}/channel/${channelId}`
