70706920d6666ac8e0b6d95b93e3a10c80e9ee2e
Ignazio Bovo <ignazio@jsgenesis.com>
2023-08-08 14:07:39 +0200

    feat: :art: initial schema generation

--- db/migrations/1691496441567-Data.js
+++ db/migrations/1691496441567-Data.js
@@ -1,5 +1,5 @@
-module.exports = class Data1691403766004 {
-    name = 'Data1691403766004'
+module.exports = class Data1691496441567 {
+    name = 'Data1691496441567'
 
     async up(db) {
         await db.query(`CREATE TABLE "channel_follow" ("id" character varying NOT NULL, "user_id" character varying, "channel_id" text NOT NULL, "timestamp" TIMESTAMP WITH TIME ZONE NOT NULL, CONSTRAINT "PK_9410df2b9a316af3f0d216f9487" PRIMARY KEY ("id"))`)
