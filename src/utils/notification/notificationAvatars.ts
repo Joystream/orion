6155946c2bcdd2c870a1b189b128da26db57524a
Theophile Sandoz <theophile.sandoz@gmail.com>
2023-11-29 11:19:19 +0100

    🤞 Fix the channel avatar url (#257)
    * Extract the asset url logic
    
    * Fix the channel avatar url
    
    * Add a script to run the mail scheduler
    
    * fix: :green_heart: ci error on globalem
    
    * Memoize `getNotificationAvatar`
    
    ---------
    
    Co-authored-by: Ignazio Bovo <ignazio@jsgenesis.com>

--- src/utils/notification/notificationAvatars.ts
+++ src/utils/notification/notificationAvatars.ts
@@ -1,39 +1,38 @@
+import { memoize } from 'lodash'
 import { EntityManager } from 'typeorm'
+
 import { Channel, MemberMetadata, StorageDataObject } from '../../model'
+import { getAssetUrls } from '../../server-extension/resolvers/AssetsResolver/utils'
 import { ConfigVariable, config } from '../config'
 
-export const getNotificationAvatar = async (
-  em: EntityManager,
-  type: 'channelId' | 'membershipId',
-  param: string
-): Promise<string> => {
-  switch (type) {
-    case 'channelId': {
-      const channel = await em.getRepository(Channel).findOneBy({ id: param })
+export const getNotificationAvatar = memoize(
+  async (em: EntityManager, type: 'channelId' | 'membershipId', param: string): Promise<string> => {
+    switch (type) {
+      case 'channelId': {
+        const channel = await em.getRepository(Channel).findOneBy({ id: param })
+        const objectId = channel?.avatarPhotoId
+        if (!objectId) break
 
-      if (!channel?.avatarPhotoId) break
+        const object = await em.getRepository(StorageDataObject).findOneBy({ id: objectId })
+        const avatarUrls = await getAssetUrls(objectId, object?.storageBagId, { limit: 1 })
+        if (!avatarUrls?.[0]) break
 
-      const avatar = await em
-        .getRepository(StorageDataObject)
-        .findOneBy({ id: channel.avatarPhotoId })
+        return avatarUrls[0]
+      }
 
-      if (!avatar || !avatar.isAccepted || !avatar.resolvedUrls[0]) break
+      case 'membershipId': {
+        const member = await em.getRepository(MemberMetadata).findOneBy({ id: param })
+        const avatar = member?.avatar
 
-      return avatar.resolvedUrls[0]
+        // AvatarObject is not yet supported
+        if (!avatar || avatar.isTypeOf !== 'AvatarUri') break
+
+        return avatar.avatarUri
+      }
     }
 
-    case 'membershipId': {
-      const member = await em.getRepository(MemberMetadata).findOneBy({ id: param })
-      const avatar = member?.avatar
-
-      // AvatarObject is not yet supported
-      if (!avatar || avatar.isTypeOf !== 'AvatarUri') break
-
-      return avatar.avatarUri
-    }
+    // Fallback to a placeholder
+    const notificationAssetRoot = await config.get(ConfigVariable.AppAssetStorage, em)
+    return `${notificationAssetRoot}/placeholder/avatar.png`
   }
-
-  // Fallback to a placeholder
-  const notificationAssetRoot = await config.get(ConfigVariable.AppAssetStorage, em)
-  return `${notificationAssetRoot}/placeholder/avatar.png`
-}
+)
