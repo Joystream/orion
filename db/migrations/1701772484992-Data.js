cd7e1aae87fbca244228d4e4faa133f8305f35f8
WRadoslaw <r.wyszynski00@gmail.com>
2023-12-05 11:35:45 +0100

    Regen migrations v3

--- db/migrations/1701772484992-Data.js
+++ db/migrations/1701772484992-Data.js
@@ -1,5 +1,5 @@
-module.exports = class Data1701767227577 {
-    name = 'Data1701767227577'
+module.exports = class Data1701772484992 {
+    name = 'Data1701772484992'
 
     async up(db) {
         await db.query(`CREATE TABLE "channel_follow" ("id" character varying NOT NULL, "user_id" character varying, "channel_id" text NOT NULL, "timestamp" TIMESTAMP WITH TIME ZONE NOT NULL, CONSTRAINT "PK_9410df2b9a316af3f0d216f9487" PRIMARY KEY ("id"))`)
