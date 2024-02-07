fbd3fb99bd8b88fb963847e94ef245c7ae9a1172
WRadoslaw <r.wyszynski00@gmail.com>
2024-02-07 14:22:58 +0100

    Add language utils

---
+++ src/utils/language.ts
@@ -0,0 +1,22 @@
+import LanguageDetect from 'languagedetect'
+
+function cleanString(input: string): string {
+  // Remove symbols, numbers, and emojis
+  // The regex [\p{P}\p{S}\p{N}\p{M}] matches all kinds of punctuation, symbols, numbers, and mark characters (including emojis)
+  // \p{P} matches any kind of punctuation character
+  // \p{S} matches any kind of math symbol, currency sign, or modifier symbol
+  // \p{N} matches any kind of numeric character in any script
+  // \p{M} matches characters that are combined with other characters, often used for emojis and diacritics
+  // The 'u' flag enables Unicode support, allowing the regex to match Unicode characters and properties
+  const cleanedString = input.replace(/[\p{P}\p{S}\p{N}\p{M}]/gu, '')
+  return cleanedString.toLowerCase()
+}
+
+const lngDetector = new LanguageDetect()
+lngDetector.setLanguageType('iso2')
+
+// Example usage
+export const predictLanguage = (text: string): string | undefined => {
+  const cleanedText = cleanString(text)
+  return lngDetector.detect(cleanedText, 1)[0]?.[0]
+}
