8b441ecebec955c5ee52d397afff6b103fee95de
Zeeshan Akram <97m.zeeshan@gmail.com>
2024-03-18 13:22:25 +0500

    [auth-api] update /request-email-confirmation-token endpoint

--- src/auth-server/handlers/requestEmailConfirmationToken.ts
+++ src/auth-server/handlers/requestEmailConfirmationToken.ts
@@ -1,25 +1,31 @@
 import express from 'express'
-import { NotFoundError, TooManyRequestsError, BadRequestError } from '../errors'
-import { components } from '../generated/api-types'
-import { globalEm } from '../../utils/globalEm'
-import { Account, Token, TokenType } from '../../model'
 import { MoreThan } from 'typeorm'
+import { Account, EmailConfirmationToken, Session } from '../../model'
 import { ConfigVariable, config } from '../../utils/config'
-import { sendWelcomeEmail } from '../utils'
+import { globalEm } from '../../utils/globalEm'
+import { BadRequestError, TooManyRequestsError } from '../errors'
+import { components } from '../generated/api-types'
+import { sendLoginOrChangePasswordEmail, sendWelcomeEmail } from '../utils'
 
 type ReqParams = Record<string, string>
 type ResBody =
   | components['schemas']['GenericOkResponseData']
   | components['schemas']['GenericErrorResponseData']
 type ReqBody = components['schemas']['RequestTokenRequestData']
+type ResLocals = { authContext: Session }
 
 export const requestEmailConfirmationToken: (
   req: express.Request<ReqParams, ResBody, ReqBody>,
-  res: express.Response<ResBody>,
+  res: express.Response<ResBody, ResLocals>,
   next: express.NextFunction
 ) => Promise<void> = async (req, res, next) => {
   try {
     const { email } = req.body
+    const { authContext } = res.locals
+
+    if (authContext.account) {
+      throw new BadRequestError('Already logged in to an account.')
+    }
 
     const em = await globalEm
 
@@ -38,18 +44,9 @@ export const requestEmailConfirmationToken: (
         lock: { mode: 'pessimistic_write' },
       })
 
-      if (!account) {
-        throw new NotFoundError('Account not found')
-      }
-
-      if (account.isEmailConfirmed) {
-        throw new BadRequestError('Email already confirmed')
-      }
-
-      const tokensInTimeframeCount = await em.getRepository(Token).count({
+      const tokensInTimeframeCount = await em.getRepository(EmailConfirmationToken).count({
         where: {
-          issuedForId: account.id,
-          type: TokenType.EMAIL_CONFIRMATION,
+          email,
           issuedAt: MoreThan(
             new Date(Date.now() - emailConfirmationTokenExpiryTimeHours * 3600 * 1000)
           ),
@@ -60,17 +57,15 @@ export const requestEmailConfirmationToken: (
         throw new TooManyRequestsError()
       }
 
-      // Deactivate all currently active email confirmation tokens for this account
-      await em.getRepository(Token).update(
-        {
-          issuedForId: account.id,
-          type: TokenType.EMAIL_CONFIRMATION,
-          expiry: MoreThan(new Date()),
-        },
-        { expiry: new Date() }
-      )
+      // Deactivate all currently active email confirmation tokens for this email
+      await em
+        .getRepository(EmailConfirmationToken)
+        .update({ email, expiry: MoreThan(new Date()) }, { expiry: new Date() })
 
-      await sendWelcomeEmail(account, em)
+      // Send email with verification token if Gateway account does not exist.
+      // Otherwise, send email to asking user to login/change password. but
+      // the API response should be the same to avoid email enumeration attack.
+      account ? await sendLoginOrChangePasswordEmail(email, em) : await sendWelcomeEmail(email, em)
     })
 
     res.status(200).json({ success: true })
