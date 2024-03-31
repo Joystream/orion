d2d1b039d46b721222cf2a45debd27c3186c54db
Zeeshan Akram <97m.zeeshan@gmail.com>
2024-03-31 22:22:01 +0000

    fix: getCumulativeHistoricalShareAllocation custom resolver

--- src/server-extension/resolvers/CreatorToken/index.ts
+++ src/server-extension/resolvers/CreatorToken/index.ts
@@ -57,7 +57,7 @@ export class TokenResolver {
       cumulativeAllocationAmount += share.allocation
     }
     return {
-      cumulativeHistoricalAllocation: Number(cumulativeAllocationAmount),
+      cumulativeHistoricalAllocation: cumulativeAllocationAmount.toString(),
     }
   }
 
