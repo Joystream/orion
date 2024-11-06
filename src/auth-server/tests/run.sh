b0cd666523180721dfb97e8b456535dcab917a21
Lezek123 <leszek@jsgenesis.com>
2024-11-06 14:05:41 +0100

    More docker compose CI fixes

--- src/auth-server/tests/run.sh
+++ src/auth-server/tests/run.sh
@@ -6,9 +6,9 @@ cd $SCRIPT_PATH/../../..
 
 if ! [[ "$(docker container inspect -f '{{.State.Running}}' orion_db)" = "true" ]]; then
     docker network create joystream_default || true
-    docker-compose up -d orion_db
-    until docker-compose logs orion_db | grep "database system is ready to accept connections"; do
-        docker-compose logs --tail 10 orion_db
+    docker compose up -d orion_db
+    until docker compose logs orion_db | grep "database system is ready to accept connections"; do
+        docker compose logs --tail 10 orion_db
         echo "Waiting for the db to be ready..."
         sleep 1
     done
